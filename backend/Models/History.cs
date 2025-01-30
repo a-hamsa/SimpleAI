95ddf374cd3ced8ffbb5dd1cab34d4acbf19bcbc
Anastasia <ahmadhamsa01@gmail.com>
2025-01-30 23:15:46 +0700

    Add User and History model's

---
+++ backend/Models/History.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+        public string Action { get; set; }
+        public DateTime ActionDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Email { get; set; }
+        public ICollection<History> Histories { get; set; }
+    }
+}
